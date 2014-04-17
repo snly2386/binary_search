e49b663f7bee40c49d250c7099d29d807561d8a2
Christopher Palmer <snly2386@gmail.com>
2014-04-17 17:17:55 -0500

    binary search

--- bsearch.rb
+++ bsearch.rb
@@ -1,20 +1,46 @@
 require 'rubygems'
 require 'pry-debugger'
 module ArrayUtil
-  def self.bsearch(array, element)
-    # If the array is empty return a result
-    # Otherwise call your helper _bsearch method
-  end
+ def self.bsearch(array, element)
 
-  def self._bsearch(array, element, start_index, end_index)
-    # If the start and end indeces are the same, search
-    # that element and return
-    #
-    # Otherwise, find the midpoint
-    #   midpoint = start_index + (end_index - start_index) / 2
-    #
-    # If the element is greater than the one at midpoint, call
-    #   _bsearch on the right half of the remaining array
-    # Otherwise, call _bsearch on left half of the remaining array
-  end
+   start_index = 0
+   end_index = array.length-1
+
+   if array.empty? == true
+     return false
+   else
+     ArrayUtil._bsearch(array, element, start_index, end_index)
+   end
+   # If the array is empty return a result
+   # Otherwise call your helper _bsearch method
+ end
+
+ def self._bsearch(array, element, start_index, end_index)
+
+   if start_index == end_index && array[start_index] == element
+     return true
+   elsif start_index == end_index
+     return false
+   end
+
+   mid_index = start_index + (end_index - start_index)/2
+   if array[mid_index] == element
+     return true
+   elsif element > array[mid_index]
+     _bsearch(array, element, mid_index+1, end_index)
+   elsif element < array[mid_index]
+     _bsearch(array, element, start_index, mid_index)
+   end
+
+
+   # If the start and end indeces are the same, search
+   # that element and return
+   #
+   # Otherwise, find the midpoint
+   #   midpoint = start_index + (end_index - start_index) / 2
+   #
+   # If the element is greater than the one at midpoint, call
+   #   _bsearch on the right half of the remaining array
+   # Otherwise, call _bsearch on left half of the remaining array
+ end
 end
